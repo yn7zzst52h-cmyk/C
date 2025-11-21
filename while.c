bfdf511b389549ddf4f03b808acd7be4bb6f5799
Rodrigo <rodrigomfantunes@gmail.com>
2025-11-21 01:38:44 +0000

    while loop

--- while.c
+++ while.c
@@ -7,6 +7,11 @@ int main() {
         printf("%d\n", index);
         index ++;
     }
+
+    do {            // aqui ele primeiro executa e so depois verifica, logo é sempre executado pelo menos uma vez
+        printf("%d\n", index);
+        index ++;
+    } while ( index <= 5 );
     getchar();
     return 0;
 }
